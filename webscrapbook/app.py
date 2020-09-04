24ed611a0842fa80103c00100e3ba56fee84c3e5
Danny Lin <danny0838@gmail.com>
2020-09-04 16:47:55 +0800

    Clear session when password changed

--- webscrapbook/app.py
+++ webscrapbook/app.py
@@ -1247,8 +1247,9 @@ def handle_before_request():
     else:
         sid = session.get('auth_id')
         try:
+            assert session.get('auth_pw') == runtime['config']['auth'][sid].get('pw', '')
             perm = runtime['config']['auth'][sid].get('permission', 'all')
-        except KeyError:
+        except (KeyError, AssertionError):
             id, perm = get_permission(auth, auth_config)
 
             # clear if session becomes invalid and no new log in
@@ -1260,6 +1261,7 @@ def handle_before_request():
     # store auth id in session
     if id:
         session['auth_id'] = id
+        session['auth_pw'] = runtime['config']['auth'][id].get('pw', '')
         session.permanent = True
 
     if not verify_authorization(perm, request.action):
